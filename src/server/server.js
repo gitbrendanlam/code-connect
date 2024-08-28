7cefec11471fd997c9bc09bf28966d7929cc0cce
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-08-27 18:58:23 -0700

    cleaned up excess js files for cleaner repo

--- src/server/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const PORT = 3000;
-const app = express();
-// set up the server to listen for http requests
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
