f76afed28ef06fd5716c0a4b287e19994c61c764
Dylan <dcarothers16@gmail.com>
2024-09-07 14:09:55 -0400

    Additional naming change

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/deez' element={<GroupFormPage/>} />
+        <Route path='/group-form' element={<GroupFormPage/>} />
         <Route path='/' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/groupdetail' element={<GroupDetails />} />
